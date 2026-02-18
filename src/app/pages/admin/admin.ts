4f946cf4cf28e549b0c8a80bf27ac0f0d64a4eb0
Natali <natasha.yashina.15@gmail.com>
2026-02-18 23:19:55 +0300

    admin

--- src/app/pages/admin/admin.ts
+++ src/app/pages/admin/admin.ts
@@ -1,11 +1,91 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ProductService, Product } from '../../services/product';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Header } from '../../components/header/header';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule, RouterModule, Header, FormsModule],
   selector: 'app-admin',
-  imports: [],
   templateUrl: './admin.html',
   styleUrl: './admin.scss',
 })
-export class Admin {
+export class Admin implements OnInit {
+  products: Product[] = [];
 
+  // Для модального окна
+  showModal = false;
+  editMode = false; // false — добавление, true — редактирование
+  currentProduct: Product = this.createEmptyProduct();
+
+  constructor(private productService: ProductService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+      this.cdr.detectChanges();
+    });
+  }
+
+  createEmptyProduct(): Product {
+    return {
+      id: 0,
+      quantity: 0,
+      name: '',
+      description: '',
+      price: 0,
+      isFavorite: false,
+    };
+  }
+
+  toggleFavorite(product: Product) {
+    product.isFavorite = !product.isFavorite;
+    this.productService.updateProduct(product).subscribe();
+  }
+
+  deleteProduct(product: Product) {
+    if (!confirm(`Удалить товар "${product.name}"?`)) return;
+    this.productService.deleteProduct(product.id!).subscribe(() => {
+      alert('Товар удалён');
+      this.loadProducts();
+    });
+  }
+
+  openAddModal() {
+    this.editMode = false;
+    this.currentProduct = this.createEmptyProduct();
+    this.showModal = true;
+  }
+
+  openEditModal(product: Product) {
+    this.editMode = true;
+    this.currentProduct = { ...product }; // создаём копию, чтобы не менять до сохранения
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  saveProduct() {
+    if (this.editMode) {
+      this.productService.updateProduct(this.currentProduct).subscribe(() => {
+        alert('Товар обновлён');
+        this.loadProducts();
+        this.closeModal();
+      });
+    } else {
+      this.productService.addProduct(this.currentProduct as any).subscribe(() => {
+        alert('Товар добавлен');
+        this.loadProducts();
+        this.closeModal();
+      });
+    }
+  }
 }
