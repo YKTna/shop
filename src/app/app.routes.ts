aad6b97e06ae1829560017c0e14d1aa50a18a429
Natali <natasha.yashina.15@gmail.com>
2026-02-18 22:11:13 +0300

    favorite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './pages/product-list/product-list';
 import { Cart } from './pages/cart/cart';
+import { Favorites } from './pages/favorites/favorites';
 
 export const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'cart', component: Cart },
+  { path: 'favorites', component: Favorites },
   { path: '**', redirectTo: '' }
 ];
--- src/app/pages/favorites/favorites.ts
+++ src/app/pages/favorites/favorites.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { finalize } from 'rxjs';
+import { OrderService } from '../../services/order';
+import { Product, ProductService } from '../../services/product';
+import { Header } from '../../components/header/header';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [CommonModule, RouterModule, Header],
   templateUrl: './favorites.html',
   styleUrl: './favorites.scss',
 })
 export class Favorites {
+
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+
+  // eslint-disable-next-line @angular-eslint/prefer-inject
+  constructor(
+    private productService: ProductService,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  private loadProducts() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+      this.filterProducts();
+    });
+  }
+
+  private filterProducts() {
+    this.filteredProducts = this.products.filter(p => p.isFavorite);
+    this.cdr.detectChanges();
+  }
+
+  decrementQuantity(product: Product) {
+    if (product.quantity > 0) {
+      product.quantity--;
+      this.productService.updateProduct(product).subscribe(() => {
+        this.filterProducts();
+      });
+    }
+  }
+
+  incrementQuantity(product: Product) {
+    product.quantity++;
+    this.productService.updateProduct(product).subscribe(() => {
+      this.filterProducts();
+    });
+  }
+
+  toggleFavorite(product: Product) {
+    product.isFavorite = !product.isFavorite;
+    this.productService.updateProduct(product).subscribe();
+    this.loadProducts();
+  }
 
 }
